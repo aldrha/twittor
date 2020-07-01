d5f2b446c5f2bfae4a66c18697ba6258eec6a982
Aldrha Petit <aldrha@gmail.com>
2020-07-01 14:09:31 -0400

    Primer comentario

---
+++ js/sw-utils.js
@@ -0,0 +1,41 @@
+
+
+function actualizaCacheDinamico( dynamicCache, req, res){
+
+    if (res.ok) {        
+    
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+
+            return res.clone();
+
+        });
+    }
+    else{
+        return res;
+    }
+
+}
+
+
+function limpiarCache(cacheName, numeroItems) {
+
+    caches.open( cacheName )
+        .then( cache => {
+
+            return cache.keys()
+                .then( keys => {
+
+                    if (keys.length > numeroItems) {
+
+                        cache.delete( keys[0])
+                            .then( limpiarCache( cacheName, numeroItems) );
+                        
+                    }
+
+                });
+
+        });
+    
+}
