9a1f88690a90d0ecc328066987e52ca20e572b9a
Aldrha Petit <aldrha@gmail.com>
2020-07-14 21:14:01 -0400

    modificaciones para ios

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE      = 'static-v2';
+const STATIC_CACHE      = 'static-v3';
 const DYNAMIC_CACHE     = 'dynamic-v1';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
@@ -44,6 +44,10 @@ self.addEventListener('activate', e => {
         keys.forEach( key => {
 
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);                
+            }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);                
             }
         });
